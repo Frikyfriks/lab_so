1d9a11942d10171822b9cd394213e1f90aaf8aeb
Frikyfriks <dario@TABLET-HTPHQRF8.localdomain>
2022-06-24 00:31:43 +0200

    Batch & Makefile

---
+++ programmiC/tools/librerie.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <pthread.h>
---
+++ programmiC/tools/quitfunction.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <pthread.h>
+
+void quit(int errNo)
+{
+    switch (errNo)
+    {
+    case 0:
+        break;
+    case 3:
+        fprintf(stderr, "Incorrect input. Usage: ./NAME <target> <n>.\n");
+        break;
+    case 4:
+        fprintf(stderr, "File already exists or directory given is invalid.\n");
+        break;
+    case 5:
+        fprintf(stderr, "Incorrect input. <n> must be between 1 and 10.\n");
+        break;
+    case 6:
+        fprintf(stderr, "Wfork failure.\n");
+        break;
+    case 7:
+        fprintf(stderr, "Pipe reading error.\n");
+        break;
+    }
+}
+
+int main(){
+}
